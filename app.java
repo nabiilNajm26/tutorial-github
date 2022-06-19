83d1e79f8ed455994dbdec905518429cc1dc3a71
Bintang Rahmatullah <rahmatbintang181@gmail.com>
2022-06-19 15:01:58 +0700

    menambahkan fitur perkalian

--- app.java
+++ app.java
@@ -1,5 +1,10 @@
 public class app {
   public static void main(String[] args) {
     System.out.println("Hello Nabil Najm!");
+
+    int a = 5;
+    int b = 10;
+
+    System.out.println(a * b);
   }
 }
